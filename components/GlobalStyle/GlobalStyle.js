21b69455b1bae2fcac591883e0b4035a67605033
Sarmad Ahmad <sarmad9@hotmail.de>
2022-03-24 17:18:11 +0100

    add logic for if no timepoints exists

---
+++ components/GlobalStyle/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-family: Roboto;
+  }
+  body{
+  }
+`;
