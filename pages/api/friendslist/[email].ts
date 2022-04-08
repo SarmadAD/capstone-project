add15bdab1e0028f3c18afbf0b43bb31405ba22b
Sarmad Ahmad <sarmad9@hotmail.de>
2022-04-08 18:52:43 +0200

    add logic for not adding duplicate friends to mongodb

--- pages/api/friendslist/[email].ts
+++ pages/api/friendslist/[email].ts
@@ -15,8 +15,7 @@ export default async function handler(request: NextApiRequest, response: NextApi
           if (getUserWithCurrentEmail === undefined || getUserWithCurrentEmail === null) {
             response.status(404).json({ error: "User with the current Email not found." });
           } else {
-            await User.updateOne({ _id: getUserWithCurrentEmail._id }, { $push: { friendsIds: [getUserWithCurrentEmail._id] } });
-            // console.log(updatedUser);
+            const updated = await User.updateOne({ _id: getUserWithCurrentEmail._id }, { $addToSet: { friendsIds: [getUserWithCurrentEmail._id] } });
             response.status(200).json(getUserWithCurrentEmail);
           }
         } else {
