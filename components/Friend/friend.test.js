81beb9b9b3af3b9724f39d40e253f03eb3de350a
Sarmad Ahmad <sarmad9@hotmail.de>
2022-04-12 11:17:29 +0200

    add tests

--- components/Friend/friend.test.js
+++ components/Friend/friend.test.js
@@ -6,7 +6,11 @@ import { render, screen } from "@testing-library/react";
 import Friend from "./Friend";
 
 const exampleFriend = {
+  id: "6245b003db2493cb8e4e7482",
   name: "Sarmad",
+  image: "imageUrl",
+  email: "sarmad@testmail.de",
+  friendsIds: ["6245b003db2493cb8e4e7482"],
 };
 
 describe("friend", () => {
@@ -15,6 +19,4 @@ describe("friend", () => {
     const friend = screen.getByTestId("friend");
     expect(friend).toBeInTheDocument();
   });
-  it.todo("test if friend item can be created");
-  it.todo("test if friend can be deleted");
 });
