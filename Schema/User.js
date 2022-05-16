3408f4339800485447f86c0782ef51242f87d4a8
SARMADSPC\sarma <sarmad9@hotmail.de>
2022-05-16 20:10:24 +0200

    add endpoint for creating invitation, add invitation enum

--- Schema/User.js
+++ Schema/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema(
     image: { type: String },
     email: { type: String },
     friendsIds: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    invitationIds: [{ type: Schema.Types.ObjectId, ref: "Invitation" }],
   },
   { timestamps: true }
 );
---
+++ pages/api/friendslist/invite/[email].ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import User from "../../../../Schema/User";
+import Invitation from "../../../../Schema/Invitation";
+import { getSession } from "next-auth/react";
+import { connectDb } from "../../../../utils/db";
+import { InviteStatus } from "../../../../utils/enum/InviteStatus";
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const { email } = request.query;
+  try {
+    connectDb();
+    const session = await getSession({ req: request });
+    switch (request.method) {
+      case "POST":
+        if (session) {
+          const getUserWithCurrentEmail = await User.findOne().where({ email: email });
+          if (getUserWithCurrentEmail === undefined || getUserWithCurrentEmail === null) {
+            response.status(404).json({ error: "Invite not send: User with the current Email not found." });
+          } else {
+            const createdInvitation = await Invitation.create({requestingUserId:session.user.id, requestedUserId:getUserWithCurrentEmail.id, status:InviteStatus.requested});
+            await User.updateOne({ _id: session.user.id }, { $addToSet: { invitationIds: [createdInvitation._id] } }); 
+            response.status(200);
+          }
+        } else {
+          response.status(401).json({ error: "Not authenticated" });
+        }
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error();
+  }
+}
